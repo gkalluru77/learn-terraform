bacd8c88c73936ecd544cdd469a360bc83db19d1
gkalluru77 <girinath.kalluru@gmail.com>
2023-12-03 16:23:20 +0530

    Expense project = Frontend interface

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -6,3 +6,11 @@ variable "sample1" {
 output "sample1" {
    value = var.sample1
 }
+
+variable "fruits" {
+   default = ["apple","mango","banana"]
+}
+
+output "fruits_first" {
+  value = var.fruits[0]
+}
